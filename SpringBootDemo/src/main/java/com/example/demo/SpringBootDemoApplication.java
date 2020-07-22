053d5500289103e5cec7db8f3f1a4b57e97c53b0
lucky <lucky@lucky-Lenovo-ideapad-520-15IKB>
2020-07-22 13:22:53 +0530

    updated

--- SpringBootDemo/src/main/java/com/example/demo/SpringBootDemoApplication.java
+++ SpringBootDemo/src/main/java/com/example/demo/SpringBootDemoApplication.java
@@ -3,12 +3,18 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/***
+ * 
+ * @author amit kumar
+ * Using Spring initializr https://start.spring.io.
+ * 
+ */
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Spring Boot Application starting!!");
 		SpringApplication.run(SpringBootDemoApplication.class, args);
+		System.out.println("\n ===================  Spring Boot Application started!! ===================");
 	}
 
 }
