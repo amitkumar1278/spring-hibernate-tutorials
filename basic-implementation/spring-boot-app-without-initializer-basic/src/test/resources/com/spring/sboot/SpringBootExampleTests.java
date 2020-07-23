6ef0f2a6e44ce829fadad9fff2258018eaefa5d0
AMIT KUMAR <amitkumar12788@gmail.com>
2020-07-23 17:46:16 +0530

    updated

---
+++ basic-implementation/spring-boot-app-without-initializer-basic/src/test/resources/com/spring/sboot/SpringBootExampleTests.java
@@ -0,0 +1,21 @@
+package com.spring.sboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/***
+ * 
+ * @author amit kumar
+ * 
+ */
+
+
+
+@SpringBootTest
+class SpringBootExampleTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
