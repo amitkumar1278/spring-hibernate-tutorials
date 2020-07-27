c0dac8a1551b56cec7e42f1c828616e574f616c3
AMIT KUMAR <amitkumar12788@gmail.com>
2020-07-27 16:09:08 +0530

    Add files via upload

---
+++ basic-examples/SpringBootHelloWorld-master/src/test/java/com/spring/sboot/SBootHelloWorldAppTests.java
@@ -0,0 +1,16 @@
+package com.spring.sboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SBootHelloWorldAppTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
