ff2a68ae209b5cd9bea5eb342ad3460a187db282
AMIT KUMAR <amitkumar12788@gmail.com>
2020-07-28 10:12:11 +0530

    Add files via upload

---
+++ sboot-profiles-db-h2/src/main/java/com/spring/sboot/SBootProfilesApplication.java
@@ -0,0 +1,14 @@
+package com.spring.sboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBootProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBootProfilesApplication.class, args);
+	}
+
+}
+
