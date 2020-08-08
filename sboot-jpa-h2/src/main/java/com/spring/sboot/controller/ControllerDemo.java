1102ae12ba46248c71c6f0c18cc37489f8491548
AMIT KUMAR <amitkumar12788@gmail.com>
2020-08-08 20:55:17 +0530

    updated

--- sboot-jpa-h2/src/main/java/com/spring/sboot/controller/ControllerDemo.java
+++ sboot-jpa-h2/src/main/java/com/spring/sboot/controller/ControllerDemo.java
@@ -1,15 +1,16 @@
 package com.spring.sboot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
-public class ControllerDemo 
-{
-	@RequestMapping("/")
-	public String home()
-	{
-		return "home.jsp";
+public class ControllerDemo {
+	
+	@RequestMapping("/home")
+//	@GetMapping("/home")
+	public String home() {
+		return "index";
 	}
+	
 }
