bc24817afdf6d0ba26914f7497316de0bae67b4a
AMIT KUMAR <amitkumar12788@gmail.com>
2020-08-17 02:19:43 +0530

    updated

---
+++ sboot-jpa-hibernate-mappings/src/main/java/com/spring/sboot/One2OneMappingStartupRunner.java
@@ -0,0 +1,88 @@
+package com.spring.sboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.spring.sboot.domains.one2one.Address;
+import com.spring.sboot.domains.one2one.User;
+import com.spring.sboot.repositories.one2one.AddressRepository;
+import com.spring.sboot.repositories.one2one.UserRepository;
+
+@Order(value = 3)
+@Component
+public class One2OneMappingStartupRunner implements CommandLineRunner {
+
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	AddressRepository adddRepository;
+
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println("\n\n=============================================================================================");
+		System.out.println("######################    One2One Mapping demonstration Started    ##########################\n\n");
+		
+		/**
+		 * Create Single a user with Address
+		 */
+		Address address3 = new Address("Permanent", "madhapur", "hyderabad", "Telangana", "500000", "India");
+		User user3 = new User("Ankit", "an@am.com", "12345", address3);
+		address3.setUser(user3);
+		user3 = userRepository.save(user3);
+
+		User u1 = userRepository.findById(user3.getId()).get();
+		System.out.println("\n\n######################    Get user by ID    ##########################");
+		System.out.println(u1.toString() + "\n\n");
+
+		
+		/**
+		 * Create multiple USer with their Address
+		 */
+		Address address1 = new Address("permanent", "patna", "patna", "Bihar", "800000", "India");
+		User user1 = new User("Amit", "am@am.com", "123", address1);
+		address1.setUser(user1);
+
+		Address address2 = new Address("Mailing", "madhapur", "hyderabad", "Telangana", "500000", "India");
+		User user2 = new User("Sumit", "su@am.com", "123", address2);
+		address2.setUser(user2);
+
+		userRepository.saveAll(Arrays.asList(user1, user2));
+
+		/**
+		 * retrieve all users into list 
+		 */
+		List<User> lstUsers2 = (List<User>) userRepository.findAll();
+		
+		System.out.println("\n\n######################    All Users are below  -> List user  ##########################");
+
+		lstUsers2.forEach(user -> {
+			System.out.println("name: "+user.getName()+", email: "+user.getEmail());
+			System.out.println(user.toString());
+			System.out.println("---------------------------------------------\n");
+		});
+
+		System.out.println("\n######################    Deleting User having id: "+ user3.getId()+" & name: "+ user3.getName()+". ##########################\n");
+		userRepository.deleteById(user3.getId());
+
+		/**
+		 * retrieve all users into iterable
+		 */
+		Iterable<User> lstUsers1 = userRepository.findAll();
+		System.out.println(
+				"\n\n######################    All Users are below  -> Iterable user  ##########################");
+		lstUsers1.forEach(System.out::println);
+
+		System.out.println("\n\n######################    One2One Mapping demonstration Ended    ##########################");
+		System.out.println("===========================================================================================\n\n");
+
+	}
+
+}
--- sboot-jpa-hibernate-mappings/src/main/java/com/spring/sboot/SbootJpaHibernateMappingApplication.java
+++ sboot-jpa-hibernate-mappings/src/main/java/com/spring/sboot/SbootJpaHibernateMappingApplication.java
@@ -1,18 +1,13 @@
 package com.spring.sboot;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import com.spring.sboot.domains.one2one.Address;
-import com.spring.sboot.domains.one2one.User;
-import com.spring.sboot.repositories.one2one.AddressRepository;
 import com.spring.sboot.repositories.one2one.UserRepository;
 
 @SpringBootApplication
@@ -25,52 +20,14 @@ public class SbootJpaHibernateMappingApplication {
 	}
 
 	/**
-	 * testing one to one mapping
+	 * testing post application start component
 	 */
+	@Order(value = 2)
 	@Bean
-	public CommandLineRunner one2oneMappinngDemo(UserRepository userRepository, AddressRepository adddRepository) {
+	public CommandLineRunner mappinngDemo(UserRepository userRepository) {
 		return args -> {
 
-			/**
-			 * Create new USer with Addresses
-			 */
-			Address address1 = new Address("permanent", "patna", "patna", "Bihar", "800000", "India");
-			User user1 = new User("Amit","am@am.com", "123", address1);
-			address1.setUser(user1);
-			//			user1 = userRepository.save(user1);
-
-			Address address2 = new Address("Mailing", "madhapur", "hyderabad", "Telangana", "500000", "India");
-			User user2 = new User("Sumit","su@am.com", "123", address2);
-			address2.setUser(user2);
-			//			user2 = userRepository.save(user2);
-
-			userRepository.saveAll(Arrays.asList(user1, user2));
-
-			/**
-			 * retrieve all users
-			 */
-			Iterable<User> users1 = userRepository.findAll();
-			List<User> users  = (List<User>) userRepository.findAll();
-			//			Optional<User> u1 = userRepository.findById(user1.getId());
-			//			System.out.println(u1.toString());
-
-			/**
-			 * print all users
-			 */
-			System.out.println("\n\n\n\n All Users are below ");
-			System.out.println("################################################");
-
-			users.forEach(user -> {
-				System.out.println("---------------------------------------------");	
-				System.out.println(user.getName());	
-				System.out.println(user.getEmail());	
-				System.out.println(user.toString());
-			});
-			
-			System.out.println("\n\n################################################");
-			users1.forEach(System.out::println);
-
-			System.out.println("\n\n\n\n ");
+			System.out.println("in mappinngDemo");
 		};
 	}
 }
