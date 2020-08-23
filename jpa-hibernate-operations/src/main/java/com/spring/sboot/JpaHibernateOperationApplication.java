112cae487efd01add17a71b3b24118dca24554b5
AMIT KUMAR <amitkumar12788@gmail.com>
2020-08-23 20:22:49 +0530

    updated

---
+++ jpa-hibernate-operations/src/main/java/com/spring/sboot/JpaHibernateOperationApplication.java
@@ -0,0 +1,13 @@
+package com.spring.sboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHibernateOperationApplication.class, args);
+	}
+
+}
---
+++ jpa-hibernate-operations/src/test/java/com/spring/sboot/JpaHibernateOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
