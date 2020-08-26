625d3d2c533ae51f8fa66ac1ab0cf68847c52052
AMIT KUMAR <amitkumar12788@gmail.com>
2020-08-26 11:25:08 +0530

    updated

---
+++ jpa-hibernate-operations/src/main/java/com/ag/sjh/pagination/repositories/PersonRepository.java
@@ -0,0 +1,16 @@
+package com.ag.sjh.pagination.repositories;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.ag.sjh.pagination.domain.Person;
+
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+	Page<Person> findByLastName(String lastName, Pageable pageable);
+
+	Slice<Person> findByAgeBetween(int start, int end, Pageable pageable);
+
+}
